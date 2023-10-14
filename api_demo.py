5085780f2f7b72c8c9bdf8dafc93ee6791398c4e
Makbel116 <makbel9395@gmail.com>
2023-10-14 03:08:08 -0700

    imported necessary libraries

---
+++ api_demo.py
@@ -0,0 +1,7 @@
+#to import the subscription_key
+import settings
+#Use the requests library to simplify making a REST API call from Python
+import requests
+# We will need the json library to read the data passed back by the web service
+import json
+
